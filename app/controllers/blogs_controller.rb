2d71969ee5dc462b3bd5ce46991f5968ba53c933
Yoshihiro Kimura <kimura@diveintocode.jp>
2018-10-02 17:56:26 +0900

    ブログのCRUDの大枠を作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,48 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
   end
 
   def show
   end
 
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      redirect_to blog_url(@blog), notice: 'ブログを投稿しました'
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
-  def new
+  def update
+    if @blog.update(blog_params)
+      redirect_to blog_url(@blog), notice: 'ブログを編集しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to root_url, notice: 'ブログを削除しました'
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.fetch(:blog, {}).permit(:title, :content)
   end
 end
